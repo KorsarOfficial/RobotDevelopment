c111b11d7346fc80041f9cae301a4bfeaa69b987
Arthur <bebrusponuhanus@gmail.com>
2025-04-13 12:26:05 +0400

    Create __init__.py

---
+++ competitions_webots/robot_control/manipulators_control/manipulators_control/pymoveit2/__init__.py
@@ -0,0 +1,5 @@
+from .gripper_command import GripperCommand
+from .gripper_interface import GripperInterface
+from .moveit2 import MoveIt2
+from .moveit2_gripper import MoveIt2Gripper
+from .moveit2_servo import MoveIt2Servo
